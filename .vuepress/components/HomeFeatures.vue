ae83cf532232cf7e4652a5617a943c219e8da170
ren-kemuri <ren@kemuri.in>
2019-11-11 20:02:07 +0530

    fixed commit

---
+++ .vuepress/components/HomeFeatures.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="features">
+      <div v-for="feature in $page.frontmatter.homeFeatures" :key="feature" class="feature">
+        <h2>{{ feature.title }}</h2>
+        <p>{{ feature.details }}</p>
+        <button>More Details</button>
+      </div>
+    </div>
+  </div>
+</template>
